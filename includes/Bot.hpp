92f5a45add17ef3ee3f309f293208898d4d35b81
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2024-06-07 21:32:55 +0100

    Add(melhadou): create boilerplate for bot

---
+++ includes/Bot.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class Bot {
+private:
+
+public:
+	Bot();
+	~Bot();
+};
---
+++ sources/Bot.cpp
@@ -0,0 +1,8 @@
+#include "Bot.hpp"
+#include <iostream>
+
+Bot::Bot() {
+	std::cout << "Bot Started" << std::endl;
+}
+
+Bot::~Bot() {}
