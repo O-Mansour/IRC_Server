3c2aca85f5e9d8593700431b32a597929ac4513d
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2024-06-10 02:44:41 +0100

    formating client

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -1,25 +1,25 @@
 #ifndef CLIENT_HPP
-# define CLIENT_HPP
+#define CLIENT_HPP
 
 #include <string>
 class client {
-	private:
-		int fd;
-		std::string nickname;
-		std::string username;
-		std::string fullname;
+private:
+  int fd;
+  std::string nickname;
+  std::string username;
+  std::string fullname;
 
-	public:
-		client(int nb);
-		~client();
-		bool authentication[3];
-		int getFd() const;
-		std::string getNickname() const;
-		std::string getUsername() const;
-		std::string getFullname() const;
-		void setNickname(const std::string n);
-		void setUsername(const std::string n);
-		void setFullname(const std::string n);
+public:
+  client(int nb);
+  ~client();
+  bool authentication[3];
+  int getFd() const;
+  std::string getNickname() const;
+  std::string getUsername() const;
+  std::string getFullname() const;
+  void setNickname(const std::string n);
+  void setUsername(const std::string n);
+  void setFullname(const std::string n);
 };
 
 #endif
