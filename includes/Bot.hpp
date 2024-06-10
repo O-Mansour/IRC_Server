eaec3dd335522bbadf66e272b12ba857fb326514
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2024-06-10 22:48:22 +0100

    add: checking for bad words in bot

--- includes/Bot.hpp
+++ includes/Bot.hpp
@@ -9,13 +9,17 @@
  * and reply with time for !time, and !quote
  */
 
+typedef std::vector<std::string>::iterator vec_it;
+
 class Bot {
 private:
+  std::vector<std::string> badWords;
   std::string fullname;
   std::string username;
   std::string nickname;
   std::string message;
   std::string res;
+  std::string badRes;
   std::vector<std::string> msgWords;
 
 public:
@@ -26,6 +30,7 @@ public:
   void setMessage(std::string message);
   std::string getResponse(std::string channelName);
   bool parseMessage();
+  bool barWordChecker();
 
   // getters
   std::string getNickname() const;
--- includes/Bot_utils.hpp
+++ includes/Bot_utils.hpp
@@ -4,5 +4,8 @@
 #include <vector>
 
 std::string cmdHelp();
+std::string cmdQuote();
+std::string getFileContent(std::string file_name);
+std::vector<std::string> extractData();
 std::vector<std::string> splitString(const std::string &str, char delim);
 void trim(std::string &line);
