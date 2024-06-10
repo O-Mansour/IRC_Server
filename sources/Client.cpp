2e15e166c08a31e7aa57d988340ee96f2b60ffc8
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2024-06-10 02:44:41 +0100

    formating client

--- sources/Client.cpp
+++ sources/Client.cpp
@@ -1,50 +1,23 @@
 #include "../includes/Client.hpp"
 #include <string>
 
-client::client(int nb) : fd(nb)
-{
-	for (int i = 0; i < 3; i++)
-		authentication[i] = false;
+client::client(int nb) : fd(nb) {
+  for (int i = 0; i < 3; i++)
+    authentication[i] = false;
 }
 
-client::~client()
-{
-}
+client::~client() {}
 
-int client::getFd() const
-{
-	return fd;
-}
+int client::getFd() const { return fd; }
 
-void client::setNickname(const std::string n)
-{
-	nickname = n;
-}
+void client::setNickname(const std::string n) { nickname = n; }
 
+std::string client::getNickname() const { return nickname; }
 
-std::string client::getNickname() const
-{
-	return nickname;
-}
+void client::setUsername(const std::string n) { username = n; }
 
-void client::setUsername(const std::string n)
-{
-	username = n;
-}
+std::string client::getUsername() const { return username; }
 
+void client::setFullname(const std::string n) { fullname = n; }
 
-std::string client::getUsername() const
-{
-	return username;
-}
-
-void client::setFullname(const std::string n)
-{
-	fullname = n;
-}
-
-std::string client::getFullname() const
-{
-	return nickname;
-}
-
+std::string client::getFullname() const { return nickname; }
